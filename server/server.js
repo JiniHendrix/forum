c8c48f0a828da8e215b3a004e5b70a4e34e7cb53
Jin Choi <jinchoi@ucdavis.edu>
2017-04-26 23:30:35 -0700

    added links

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 const port = process.env.port || 3000;
 
+app.get('/', (req, res) => {
+  res.redirect('/login');
+})
+
 app.get('/login', (req, res) => {
-  fs.readFile('./login.html', (err, html) => {
+  fs.readFile(path.join(__dirname, '../client/login.html'), (err, html) => {
     res.set({'Content-Type':'text/html'}).send(html);
   });
 });
 
 app.get('/signup', (req, res) => {
-  fs.readFile('./signup.html', (err, html) => {
+  fs.readFile(path.join(__dirname, '../client/signup\.html'), (err, html) => {
     res.set({'Content-Type':'text/html'}).send(html);
   });
 });
